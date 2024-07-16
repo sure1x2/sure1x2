23d87446db07f03c7f66b8f71829499e8e8973e9
sure1x2 <yogadaniel256@gmail.com>
2024-07-16 21:04:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('input[name="package"]').forEach(function(input) {
+        input.addEventListener('change', function() {
+            alert('Please pay the amount on 0792398547 or 0787387157 and send a screenshot as proof of payment on our WhatsApp platform to get your odds.');
+        });
+    });
+});
